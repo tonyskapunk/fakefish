dc410341e3427e09b9419083b5adf2e647c4aa8c
Tony Garcia <tonyg@redhat.com>
2024-01-30 14:03:37 -0600

    Modify power on/off with virtctl
    It has been found that is more reliable to use virtctl to get the right
    status of the VM instead of it's CR.

--- kubevirt_scripts/common.sh
+++ kubevirt_scripts/common.sh
@@ -1,57 +1,47 @@
+# Stop VM
+# Always attempt to power off the VM, regardless of its current state.
+# Capture the virtctl output that validates the state of the VM
+# Retry until powered off or reach the max retries
 function stop_vm() {
   MAX_RETRIES=5
-  TRIES=0
-  VM_RUNNING=$(oc -n ${VM_NAMESPACE} get vm ${VM_NAME} -o jsonpath='{.spec.running}')
-  if [ $? -ne 0 ]; then
-    echo "Failed to get VM power state."
-    return 1
-  fi
-  if [[ "${VM_RUNNING}" == "false" ]]; then
-    echo "VM is already powered off"
-  else
-    while [[ ${VM_RUNNING} != "false" ]]
-    do
-      virtctl -n ${VM_NAMESPACE} stop ${VM_NAME}
-      if [ $? -eq 0 ]; then
-        return 0
-      else
-        if [[ ${TRIES} -ge ${MAX_RETRIES} ]];then
-          echo "Failed to poweroff VM"
-          return 1
-        fi
-        TRIES=$((TRIES + 1))
-        echo "Failed to poweroff VM. Retrying in 5 seconds. Retry [${TRIES}/${MAX_RETRIES}]"
-        sleep 5
+  TRIES=1
+
+  virtctl -n ${VM_NAMESPACE} stop ${VM_NAME}
+  while true; do
+    output=$(virtctl -n ${VM_NAMESPACE} stop ${VM_NAME} 2>&1 >/dev/null || true)
+    if grep -q "VM is not running" <<< "${output}"; then
+      return 0
+    else
+      if [[ ${TRIES} -ge ${MAX_RETRIES} ]]; then
+        return 1
       fi
-    done
-  fi
+      TRIES=$(( ${TRIES} + 1 ))
+      echo "Waiting to poweroff VM. Retrying in 5 seconds. Retry [${TRIES}/${MAX_RETRIES}]"
+      sleep 5
+    fi
+  done
 }
 
+# Start VM
+# Always attempt to power on the VM, regardless of its current state.
+# Capture the virtctl output that validates the state of the VM
+# Retry until ready or reach the max retries
 function start_vm() {
   MAX_RETRIES=5
-  TRIES=0
-  VM_RUNNING=$(oc -n ${VM_NAMESPACE} get vm ${VM_NAME} -o jsonpath='{.spec.running}')
-  if [ $? -ne 0 ]; then
-    echo "Failed to get VM power state."
-    return 1
-  fi
-  if [[ "${VM_RUNNING}" == "true" ]]; then
-    echo "VM is already running"
-  else
-    while [[ ${VM_RUNNING} != "true" ]]
-    do
-      virtctl -n ${VM_NAMESPACE} start ${VM_NAME}
-      if [ $? -eq 0 ]; then
-        return 0
-      else
-        if [[ ${TRIES} -ge ${MAX_RETRIES} ]];then
-          echo "Failed to poweron VM"
-          return 1
-        fi
-        TRIES=$((TRIES + 1))
-        echo "Failed to poweron VM. Retrying in 5 seconds. Retry [${TRIES}/${MAX_RETRIES}]"
-        sleep 5
+  TRIES=1
+
+  virtctl -n ${VM_NAMESPACE} start ${VM_NAME} || true
+  while true; do
+    output=$(virtctl -n ${VM_NAMESPACE} start ${VM_NAME} 2>&1 >/dev/null || true)
+    if grep -q "VM is already running" <<< "${output}"; then
+      return 0
+    else
+      if [[ ${tries} -ge ${max_tries} ]]; then
+        return 1
       fi
-    done
-  fi
+      tries=$(( ${tries} + 1 ))
+      echo "Waiting to poweron VM. Retrying in 5 seconds. Retry [${TRIES}/${MAX_RETRIES}]"
+      sleep 5
+    fi
+  done
 }
